fc28b401d34b5ca6d80833d0c7c76d8ee4d469bd
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-07-07 15:08:23 +0300

    4-commit-reducer (my version)

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import './App.css';
-import {apiPlaceHolder, getPlaceHolderObjectType} from './api/apiPlaceHolder';
+import {apiPlaceHolder, getPlaceHolderObjectType, instance} from './api/apiPlaceHolder';
+import {getAC, JsonPlaceHolderReducer} from './reduser/JsonPlaceHolderReducer';
 
 
 function App() {
 
-  const [posts, setPosts] = useState<getPlaceHolderObjectType[]>([]);
+    const [posts, dispatchPosts] = useReducer(JsonPlaceHolderReducer, []);
 
-  // const getPlaceHolderAPI = async () => {
-  //   const result = await fetch('https://jsonplaceholder.typicode.com/posts');
-  //   const data = await result.json();
-  //   setPosts(data);
-  // }
+    // const getPlaceHolderAPI = async () => {
+    //   const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+    //   const data = await result.json();
+    //   setPosts(data);
+    // }
 
-  useEffect(() => {
-    apiPlaceHolder.get()
-        .then(res => {
-          setPosts(res.data)
-        })
-  }, [])
+    useEffect(() => {
+        apiPlaceHolder.get()
+            .then(res => {
+                dispatchPosts(getAC(res.data))
+            })
+    }, [])
 
-  return (
-      <div className="App">
-        <h1>Second version</h1>
-        {posts.map(element => {
-          return (
-              <div key={element.id}>
-                <span>{element.id}-</span>
-                <span>{element.title}</span>
-              </div>
-          )
-        })}
-      </div>
-  );
+    return (
+        <div className="App">
+            <h1>Second version</h1>
+            {posts.map(element => {
+                return (
+                    <div key={element.id}>
+                        <span>{element.id}-</span>
+                        <span>{element.title}</span>
+                    </div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
